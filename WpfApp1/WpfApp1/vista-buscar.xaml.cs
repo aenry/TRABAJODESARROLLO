cd2f420aefb549041467a632c9be55837deac6bf
Victor Henriquez <dnaenry@gmail.com>
2018-04-05 09:53:47 -0300

    PRUEBA

---
+++ WpfApp1/WpfApp1/vista-buscar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para vista_buscar.xaml
+    /// </summary>
+    public partial class vista_buscar : Window
+    {
+        public vista_buscar()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
